5964d6397e36c65597eb666172208c9a6cfa813a
chowaaachi <chowaaachi@gmail.com>
2021-09-19 00:57:49 +0900

    add Particle System

---
+++ Assets/Scripts/PSO/Particle.cs
@@ -0,0 +1,118 @@
+using UnityEngine;
+using DG.Tweening;
+
+namespace PSO
+{
+    public class Particle : MonoBehaviour
+    {
+        const float defaultPositionRange = 100f;
+        const float defaultVelocityRange = 100f;
+
+        Transform cachedTransform = default;
+
+        [SerializeField, Header("次の到着地点")]
+        Vector3 nextArrivalPosition = default;
+        [SerializeField, Header("現在速度")]
+        Vector3 currentVelocity     = default;
+
+        [SerializeField, Header("粒子最適地点")]
+        Vector3 personalBestPosition    = default;
+        [SerializeField, Header("粒子最適地点スコア")]
+        float personalBestPositionScore = 0f;
+
+        /// <summary>
+        /// Awake.
+        /// ※アクセス回数が多いのでTransformのキャッシュ化.
+        /// </summary>
+        void Awake()
+        {
+            cachedTransform = transform;
+        }
+
+        /// <summary>
+        /// 初期化.
+        /// ※初期値、初期速度のランダム設定.
+        /// </summary>
+        public void Init()
+        {
+            cachedTransform.position = new Vector3(Random.Range(-defaultPositionRange, +defaultPositionRange),
+                                                   Random.Range(-defaultPositionRange, +defaultPositionRange),
+                                                   Random.Range(-defaultPositionRange, +defaultPositionRange));
+
+            currentVelocity          = new Vector3(Random.Range(-defaultVelocityRange, +defaultVelocityRange),
+                                                   Random.Range(-defaultVelocityRange, +defaultVelocityRange),
+                                                   Random.Range(-defaultVelocityRange, +defaultVelocityRange));
+        }
+
+        /// <summary>
+        /// 次のイテレートを開始.
+        /// </summary>
+        public void startNextIteration()
+        {
+            updateVelocity();
+            updateArrivalPosition();
+            startDOMove();
+        }
+
+        /// <summary>
+        /// 速度の更新.
+        /// </summary>
+        void updateVelocity()
+        {
+            var globalBestPosition = ParticleManager.Instance.GlobalBestPosition;
+
+            currentVelocity = (ParticleManager.Instance.INERTIA_COEFFICIENT       * currentVelocity)                                                              +
+                              (ParticleManager.Instance.PERSONAL_BEST_COEFFICIENT * Random.Range(0.0f, 1.0f) * (personalBestPosition - cachedTransform.position)) +
+                              (ParticleManager.Instance.GLOBAL_BEST_COEFFICIENT   * Random.Range(0.0f, 1.0f) * (globalBestPosition   - cachedTransform.position)) ;
+        }
+
+        /// <summary>
+        /// 次の到着地点の更新.
+        /// </summary>
+        void updateArrivalPosition()
+        {
+            nextArrivalPosition = cachedTransform.position + currentVelocity;
+        }
+
+        void startDOMove()
+        {
+            cachedTransform.DOMove(nextArrivalPosition, ParticleManager.Instance.DOTWEEN_DURATION).OnComplete( afterDOMove );
+        }
+
+        void afterDOMove()
+        {
+            updateBestPosition();
+
+            outputLog();
+        }
+
+        /// <summary>
+        /// 最適解の更新.
+        /// </summary>
+        void updateBestPosition()
+        {
+            float currentPositionScore = ParticleManager.GetPositionScore(cachedTransform.position);
+            if (  currentPositionScore > personalBestPositionScore)
+            {
+                personalBestPosition      = cachedTransform.position;
+                personalBestPositionScore = currentPositionScore;
+
+                ParticleManager.UpdateGlobalBestPosition(personalBestPosition, personalBestPositionScore);
+            }
+        }
+
+        /// <summary>
+        /// ログの出力.
+        /// </summary>
+        void outputLog()
+        {
+            Debug.Log(gameObject.name);
+
+            Debug.Log(nextArrivalPosition);
+            Debug.Log(currentVelocity);
+
+            Debug.Log(personalBestPosition);
+            Debug.Log(personalBestPositionScore);
+        }
+    }
+}
---
+++ Assets/Scripts/PSO/ParticleManager.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PSO
+{
+    public class ParticleManager : MonoBehaviour
+    {
+        public static ParticleManager Instance = null;
+
+        [SerializeField, Header("DOTween実行間隔"), Range(0.0f, 5.0f)]
+        public float DOTWEEN_DURATION = 1.0f;
+
+        [SerializeField, Header("慣性係数"), Range(0.0f, 1.0f)]
+        public float INERTIA_COEFFICIENT = 0.9f;
+        [SerializeField, Header("粒子最適解の参照係数"), Range(0.0f, 1.0f)]
+        public float PERSONAL_BEST_COEFFICIENT = 0.6f;
+        [SerializeField, Header("粒子群最適解の参照係数"), Range(0.0f, 1.0f)]
+        public float GLOBAL_BEST_COEFFICIENT = 0.3f;
+
+        [SerializeField, Header("粒子群")]
+        List<Particle> particles = new List<Particle>();
+
+        [SerializeField, Header("最良地点")]
+        public Transform target = null;
+
+        [NonSerialized]
+        public Vector3 GlobalBestPosition = Vector3.zero;
+        [NonSerialized]
+        public float GlobalBestPositionScore = float.MinValue;
+
+        [ContextMenu("GetParticles")]
+        public void GetParticles()
+        {
+            particles = new List<Particle>(transform.GetComponentsInChildren<Particle>());
+        }
+
+        void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(this);
+                return;
+            }
+
+            Instance = this;
+        }
+
+        void Start()
+        {
+            particles.ForEach(particle => particle.Init());
+        }
+
+        float timeCount = 0.0f;
+
+        void Update()
+        {
+            timeCount += Time.deltaTime;
+            if (timeCount >= DOTWEEN_DURATION)
+            {
+                particles.ForEach(particle => particle.startNextIteration());
+                timeCount = 0.0f;
+            }
+        }
+
+        /// <summary>
+        /// 座標の評価を取得.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public static float GetPositionScore(Vector3 position)
+        {
+            return 1.0f / (Instance.target.localPosition - position).sqrMagnitude;
+        }
+
+        /// <summary>
+        /// 粒子群最適解の更新.
+        /// </summary>
+        /// <param name="newBestPosition"></param>
+        /// <param name="newBestPositionScore"></param>
+        public static void UpdateGlobalBestPosition(Vector3 newBestPosition, float newBestPositionScore)
+        {
+            if (newBestPositionScore > Instance.GlobalBestPositionScore)
+            {
+                Instance.GlobalBestPosition      = newBestPosition;
+                Instance.GlobalBestPositionScore = newBestPositionScore;
+
+                Debug.Log("Updated!");
+                Debug.Log(newBestPosition);
+            }
+        }
+    }
+}
